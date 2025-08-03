efa1f7d8a4d06d1a1e8d35549ef6b16d16e7261b
Jahir514 <jahirhasan.amana@gmail.com>
2025-08-03 23:45:44 +0600

    income create service method modifiy

--- src/services/income.service.ts
+++ src/services/income.service.ts
@@ -6,14 +6,14 @@ import {
   IIncome,
   IUpdateIncome,
   IUpdateIncomeResponse,
-} from '../interfaces/income.interface';
-import Income from '../model/income.model';
-import { AppError } from '../utils/AppError';
+} from "../interfaces/income.interface";
+import Income from "../model/income.model";
+import { AppError } from "../utils/AppError";
 
 //Income create service
 const createIncome = async (data: ICreateIncome): Promise<ICreateIncomeResponse> => {
   const { title, category, amount, date } = data;
-  if (!title || !category || !amount || !date) throw new AppError('Please provide correct Income information.', 400);
+  if (!title || !category || !amount || !date) throw new AppError("Please provide correct Income information.", 400);
 
   //create ingridents instance
   const IncomeInfo = {
@@ -27,29 +27,29 @@ const createIncome = async (data: ICreateIncome): Promise<ICreateIncomeResponse>
   const savedIncome = await income.save();
   //handle response
   if (savedIncome) {
-    const incomeData = await Income.findOne({ _id: savedIncome._id });
+    const incomeData = await Income.findOne({ _id: savedIncome._id }).populate("category", "title");
     return {
       success: true,
-      message: 'Successfully create Income',
+      message: "Successfully create Income",
       income: incomeData,
     };
   } else {
-    throw new AppError('Failed to create Income', 400);
+    throw new AppError("Failed to create Income", 400);
   }
 };
 
 //all Income get service
 const getAllIncome = async (): Promise<IGetIncomeResponse> => {
-  const income: IIncome[] = await Income.find().populate('category', 'title');
+  const income: IIncome[] = await Income.find().populate("category", "title");
 
   if (income.length == 0) {
     return {
-      message: 'No income found.',
+      message: "No income found.",
       income,
     };
   } else {
     return {
-      message: '',
+      message: "",
       income,
     };
   }
@@ -59,11 +59,11 @@ const getSingleIncome = async (IncomeId: string): Promise<IGetIncomeResponse> =>
   const income: IIncome | null = await Income.findOne({ _id: IncomeId });
   if (income) {
     return {
-      message: '',
+      message: "",
       income,
     };
   } else {
-    throw new AppError('No Income found.', 400);
+    throw new AppError("No Income found.", 400);
   }
 };
 
@@ -73,23 +73,23 @@ const updateIncome = async (incomeId: string, data: IUpdateIncome): Promise<IUpd
   const income: IIncome | null = await Income.findOne({ _id: incomeId });
   //throw error if not exist
   if (!income) {
-    throw new AppError('No ingridents found', 400);
+    throw new AppError("No ingridents found", 400);
   }
   //update when it exist
   const option = { new: true, runValidator: true };
   const updatedIncome: IIncome | null = await Income.findByIdAndUpdate(incomeId, { $set: data }, option).select(
-    'name serialNo supplier category stock costPrice salePrice unit'
+    "name serialNo supplier category stock costPrice salePrice unit"
   );
   if (updatedIncome) {
     return {
       success: true,
-      message: 'Successfully update Income ',
+      message: "Successfully update Income ",
       income: updatedIncome,
     };
   } else {
     return {
       success: false,
-      message: 'Failed to update Income ',
+      message: "Failed to update Income ",
       income: updatedIncome,
     };
   }
@@ -100,20 +100,20 @@ const deleteIncome = async (incomeId: string): Promise<IDeleteIncomeResponse> =>
   const income: IIncome | null = await Income.findOne({ _id: incomeId });
   //throw error if not exist
   if (!income) {
-    throw new AppError('No Income  found', 400);
+    throw new AppError("No Income  found", 400);
   }
   //delete when it exist
-  const deletedIncome: IIncome | null = await Income.findByIdAndDelete(incomeId).select('name serialNo');
+  const deletedIncome: IIncome | null = await Income.findByIdAndDelete(incomeId).select("name serialNo");
   //handle response
   if (deletedIncome) {
     return {
       success: true,
-      message: 'Successfully delete Income. ',
+      message: "Successfully delete Income. ",
     };
   } else {
     return {
       success: false,
-      message: 'Failed to delete Income ',
+      message: "Failed to delete Income ",
     };
   }
 };
